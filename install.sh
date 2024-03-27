321f0f335ecde874fa5177b3b1a6dbc633cae16e
Arcmosh <arcmosh@vivaldi.net>
2024-03-28 01:38:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -147,8 +147,8 @@ echo
 
 echo "---------- Vmess URL ----------"
 temp_url='{"add":"IP","aid":"0","alpn":"","fp":"","host":"","id":"${uuid}","net":"tcp","path":"","port":"VMESSPORT","ps":"Vmess_${ip}_TIME","scy":"auto","sni":"","tls":"","type":"","v":"2"}'
-O_vmess_url=$(sed -e "s/IP/${ip}/g" -e "s/VMESSPORT/${vmessport}/g" -e "s/TIME/$(date +%H%M)/g" <<< "$vmess_url")
-echo -e "${o_vmess_url}"
+O_vmess_url=$(sed -e "s/IP/${ip}/g" -e "s/VMESSPORT/${vmessport}/g" -e "s/TIME/$(date +%H%M)/g" <<< "$temp_url")
+echo -e "$o_vmess_url"
 
 echo "以上节点信息保存在 ~/_vless_reality_url_ 中"
 

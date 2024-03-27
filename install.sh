2be20284c769c8d34d6e72fdcbd71a37789c03ec
Arcmosh <arcmosh@vivaldi.net>
2024-03-28 01:36:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -145,9 +145,10 @@ vless_reality_url="vless://${uuid}@${ip}:${port}?flow=xtls-rprx-vision&encryptio
 echo -e "${vless_reality_url}"
 echo
 
-echo "---------- VMess URL ----------"
-vmess_url='{"add":"${ip}","aid":"0","alpn":"","fp":"","host":"","id":"${uuid}","net":"tcp","path":"","port":"${vmessport}","ps":"Reality_${ip}_$(date +%H%M)","scy":"auto","sni":"","tls":"","type":"","v":"2"}'
-echo -e "${vless_reality_url}"
+echo "---------- Vmess URL ----------"
+temp_url='{"add":"IP","aid":"0","alpn":"","fp":"","host":"","id":"${uuid}","net":"tcp","path":"","port":"VMESSPORT","ps":"Vmess_${ip}_TIME","scy":"auto","sni":"","tls":"","type":"","v":"2"}'
+O_vmess_url=$(sed -e "s/IP/${ip}/g" -e "s/VMESSPORT/${vmessport}/g" -e "s/TIME/$(date +%H%M)/g" <<< "$vmess_url")
+echo -e "${o_vmess_url}"
 
 echo "以上节点信息保存在 ~/_vless_reality_url_ 中"
 
